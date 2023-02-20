ff644a8c0146d559f79d4857f83215d464da1eff
zyzhuaidan <1963806558@qq.com>
2023-02-20 19:30:47 +0800

    init

---
+++ src/main/java/com/xingzhou/bigdata_note/BigDataNoteApplication.java
@@ -0,0 +1,13 @@
+package com.xingzhou.bigdata_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xingzhou/bigdata_note/BigDataNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xingzhou.bigdata_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
